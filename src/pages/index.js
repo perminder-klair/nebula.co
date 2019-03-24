0cfb30b457bc12da1f450bdaeae9658bbba5038a
gagansaini1212 <gagansaini1212@gmail.com>
2019-03-24 07:52:17 +0545

    Title, subtitle and Button added to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import config from '../utils/config';
 import Seo from '../components/Seo';
-import Layout from '../components/Layout';
+
+const Container = styled.section`
+  min-height: 118vh !important;
+
+  background-image: url('../images/background.png');
+  background-repeat: no-repeat;
+  background-position: 81% 0%;
+`;
+
+const Wrapper = styled.div`
+  margin-top: 56px;
+  margin-left: 64px;
+  img {
+    width: 176px;
+    height: 54px;
+  }
+`;
+
 
 export default class IndexPage extends React.Component {
   render() {
     return (
-      <Layout>
+      <React.Fragment>
         <Seo
           title="Home"
           description="Welcome to Nebula"
           url={`${config.siteUrl}`}
         />
-        <section className="hero is-success is-fullheight">
+        <Container className="hero is-fullheight">
+          <Wrapper>
+            <img src="/images/logo.png" alt="logo" />
+          </Wrapper>
           <div className="hero-body">
             <div className="container">
-              <h1 className="title">Fullheight title</h1>
-              <h2 className="subtitle">Fullheight subtitle</h2>
+              <h1 className="has-text-weight-bold">
+                We are digital product creators
+              </h1>
+              <h2>From concept phase to customer validation, all the way through to developing the finished product on the app store</h2>
+              <a className="button is-danger is-rounded">Lets's talk</a>
             </div>
           </div>
-        </section>
-      </Layout>
+        </Container>
+      </React.Fragment>
     );
   }
 }
